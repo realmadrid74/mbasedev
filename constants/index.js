fc72af5bbc93e145859862b8bb3d2876fa547652
real boy <140603936+realmadrid74@users.noreply.github.com>
2023-07-31 11:21:35 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x95dbcafaf6473b55abbd68fd997e234418f61381";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
